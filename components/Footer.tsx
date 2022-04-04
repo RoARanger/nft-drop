50a3c3f8f3e126229fe2042e60b2cd7764529a89
RoARanger <63724883+RoARanger@users.noreply.github.com>
2022-04-04 16:28:15 +0200

    final commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="border-t border-pink-400/[0.15] bg-white/25 py-6 text-center font-poppins text-sm font-medium uppercase dark:bg-black/25 dark:text-white">
-      made by <span className="text-purple-400">butters</span>
+      made by <span className="text-purple-400">Blxck Beard</span>
     </footer>
   )
 }
